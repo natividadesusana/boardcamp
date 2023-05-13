0d23e8ede6f5fb8f8c92cf17f713a83d933770aa
natividadesusana <susanajdsn@gmail.com>
2023-05-12 21:25:19 -0300

    fix: settings

--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,12 +1,12 @@
-import { Router } from 'express';
-import { createRental, getRentals, updateRental } from '../controllers/rentals.controller.js';
-import validateSchema from '../middlewares/validateSchema.middleware.js'
-import rentalSchema from '../schemas/rentals.schemas.js'
+import { Router } from "express";
+import { createRental, getRentals, updateRental } from "../controllers/rentals.controller.js";
+import validateSchema from "../middlewares/validateSchema.middleware.js";
+import rentalSchema from "../schemas/rentals.schemas.js";
 
 const rentalsRouter = Router();
 
-rentalsRouter.post('/rentals', validateSchema(rentalSchema), createRental);
-rentalsRouter.get("/rentals", getRentals)
-rentalsRouter.post('/rentals/:id/return', updateRental);
+rentalsRouter.get("/rentals", getRentals);
+rentalsRouter.post("/rentals", validateSchema(rentalSchema), createRental);
+rentalsRouter.post("/rentals/:id/return", updateRental);
 
 export default rentalsRouter;
